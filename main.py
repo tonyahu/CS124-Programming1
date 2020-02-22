b91a35fe57a677eb63548abdd257291d4f928b73
Tony Hua <tonyhua18@gmail.com>
2020-02-22 15:51:58 -0800

    updated main program

--- main.py
+++ main.py
@@ -27,51 +27,50 @@
 # output: average numpoints numtrials dimension
 
 
-# tips from lecture7
-# path compression: instead of jumping through multiple pathways to find
-# root, we just re-assign a new pathway directly pointing to root. At a
-# constant cost of that extra step, we save ourselves the possibility of a
-# larger magnitude cost with large pathways.
-
 
 ##### IMPORT ######
 import numpy as np
 
-##### FUNCTIONS #####
 
+##### HELPER FUNCTIONS #####
 
-class graph:
+class Graph:
     def __init__(self, dimensionType, numpoints):
        self.dimensionType = dimensionType
        self.numpoints = numpoints
        self.graph = []
 
+
     def addEdge(self, u, v, w):
        self.graph.append([u, v, w])
 
-    # make a new set at each node. Each set consists of parent identifiers and
-    # ranking
+    # make a new set at each node. Each set consists of parent identifiers and ranking
     def makeSet(self, node):
         parent[node] = node  # set parent pointer to itself
         rank[node] = 0  # set node rank to 0
 
     # recursively goes up tree until root is located. Then returns root.
-    def find(self, parent, node):
-        if parent[node] == node:  # if node is a root, we are done. return the value
+    def find(self, p, node):
+        if p[node] == node:  # if node is a root, we are done. return the value
            return node 
-        return self.find(parent, parent[node]) #else, keep going upwards.
+        return self.find(p, p[node]) #else, keep going upwards.
 
     # links a small tree to larger tree. Increases its rank, and returns the new larger tree
     def link(self, node1, node2):
-        if rank[node1] > rank[node2]:
+        if r[node1] > r[node2]:
             temp = node1 
             node1 = node2   #we swap values of node1 ↔ node2
             node2 = temp
-        if rank[node1] == rank[node2]:
-            rank[node2] = rank[node2] +1    #if node ranks are same, then we increase rank node2. Else, node2 should have higher rank than node1 already from swap.
-        parent[x] = node2   #then we increase rank of node1 to match node2 after linking.
+        if r[node1] == r[node2]:
+            r[node2] = r[node2] +1    #if node ranks are same, then we increase rank node2. Else, node2 should have higher rank than node1 already from swap.
+        p[x] = node2   #then we increase rank of node1 to match node2 after linking.
         return node2
 
+
+    def union(self, node1, p1, node2, p2):
+        x = self.find(self,p1,node1)
+        y = self.find(self,p2,node2)
+        self.link(self, x, y)
 
     def makeRandomGraph(self, dimension, numpoints):
         graph = [] #init empty graph
@@ -125,3 +124,30 @@ class graph:
                 # edge weights calculated via pythagorean theorem
         else:
             print("Error: Please input value 0,2,3, or 4 for dimension. Life is hard as is already. Please don't make it harder.")
+
+
+
+def makeMST(graph):
+    print("making MST")
+    #make MST from graph.
+    #return MST
+
+#calc average weight of MST
+def calcRunTime(MST):
+    print("calculating Time")
+    return 1 #temp returning hardcoded 1
+
+
+
+
+##### MAIN PROGRAM #####
+def main(switch, numpoints, numtrials, dimension):
+    average = calcRunTime(1)
+    print("Program completed for %d vertices under case %d:", numpoints, dimension)
+    print("Average weight: %d" %average)
+    print("Numpoints: %d" %numpoints)
+    print("Numtrials: %d" %numtrials)
+    print("Dimension: %d" %dimension)
+
+##### INIT #####
+if __name__ == "__main__": main(1,2,3,4)
